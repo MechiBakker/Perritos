c9077d3ca539cf0e2197454b174deb089ab8c408
Mechi Bakker <93297328+MechiBakker@users.noreply.github.com>
2025-07-02 16:26:56 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -129,7 +129,7 @@ document.getElementById("boton-ir-transito").onclick = function() {
       // Esperar que Instagram cargue bien sus iframes
       setTimeout(() => {
         ajustarAlturaInstagram();
-      }, 1500);
-    }, 1000);
+      }, 2500);
+    }, 500);
   });
 
